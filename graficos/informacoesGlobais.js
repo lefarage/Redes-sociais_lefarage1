8d85379cc8bb24034fb5ff6af8a691c8e4bd25ee
Letícia Farage <162373412+lefarage@users.noreply.github.com>
2024-09-27 06:25:48 -0300

    +++

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,3 +1,5 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
+const url = https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json
 console.log(url);
+async function visualizarinformacoesGlobais() {
+    const res = await fetch(url)
+}
