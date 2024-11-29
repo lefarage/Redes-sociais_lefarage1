e0f312938b8b2fdcddbfa0428c3e21fd278e942b
Letícia Farage <162373412+lefarage@users.noreply.github.com>
2024-11-29 08:32:19 +0000

    informacoes globais

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,5 +1,17 @@
-const url = https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json
-console.log(url);
-async function visualizarinformacoesGlobais() {
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    
+    paragrafo.innerHTML = `O mundo tem aproximadamente <span>${dados.total_pessoas_mundo}</span> de pessoas e das quais <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    console.log(paragrafo)
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
+
+vizualizarInformacoesGlobais()
