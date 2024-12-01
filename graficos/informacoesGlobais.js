b689f6ab3b7bffc957fbedaf7af66dd81b1ebdc6
Letícia Farage <162373412+lefarage@users.noreply.github.com>
2024-12-01 17:51:10 +0000

    tofixed 3

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,8 +3,8 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas/1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo/1e9)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9). toFixed(3)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9).toFixed(3)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
@@ -13,7 +13,7 @@ async function vizualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     
-    paragrafo.innerHTML = `O mundo tem aproximadamente <span>${dados.total_pessoas_mundo}</span> bilões de pessoas e das quais <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> conectadas.`
+    paragrafo.innerHTML = `O mundo tem aproximadamente <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas e das quais <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> conectadas.`
     
 
     const container = document.getElementById('graficos-container')
